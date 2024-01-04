a507f9609287abaef10df460390c8d6184f3dbc3
ruby0143 <mysoresriharsha07@gmail.com>
2024-01-04 18:39:27 +0530

    Initial backend setup with all main routes

---
+++ index.js
@@ -0,0 +1,215 @@
+var express = require('express');
+var cors = require('cors');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const products = require('./products')
+
+const mongoUrl = `mongodb+srv://ruby07:8074662205s@cluster0.97u8x.mongodb.net/gaanjUsers`
+
+mongoose.connect(mongoUrl, { useNewUrlParser: true });
+
+const userSchema = {
+    uid: String,
+    email: String,
+    firstName: String,
+    lastName: String,
+    mobile: String,
+    address: String
+}
+
+const cartSchema = {
+    uid: String,
+    cartItems: Array
+}
+
+
+const productSchema = {
+    id: String,
+    price: Number,
+    header: Object,
+    about: Object,
+    useCases: Object,
+    testimonials: Array
+}
+
+const Users = new mongoose.model("User", userSchema);
+const Products = new mongoose.model("Product", productSchema);
+const Cart = new mongoose.model("Cart", cartSchema);
+
+
+let app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+app.post("/getUser", (req, res) => {
+    let userid = req.body.uid;
+    Users.findOne({ uid: userid })
+        .then(resp => {
+            console.log(resp, userid, "backend")
+            res.status(200).json({ message: resp })
+        })
+        .catch(err => [
+            res.status(500).json({ message: err })
+        ])
+})
+app.post("/updateUser", (req, res) => {
+    const body = req.body;
+    console.log(body, "printing...")
+    const userid = body.uid;
+    const firstName = body.firstName;
+    let lastName = body.lastName;
+    let mobile = body.mobile;
+    let address = body.address;
+    Users.updateOne({ uid: userid }, {
+        $set: {
+            firstName: firstName,
+            lastName: lastName,
+            mobile: mobile,
+            address: address
+        }
+    })
+        .then(resp => {
+            res.status(200).json({ message: resp })
+        })
+        .catch(err => {
+            console.log(err);
+        })
+})
+
+app.post("/addToCart", (req, res) => {
+    const pid = req.body.pid;
+    const uid = req.body.uid;
+    const pdata = products.products[pid - 1];
+    let currentCart = [];
+
+    Cart.findOne({ uid: uid })
+        .then(resp => {
+            currentCart = resp.cartItems;
+            let flag = 0;
+            console.log(currentCart, "printing")
+            currentCart.forEach((ele, idx) => {
+                if(ele.pid === pid){
+                    ele.quantity = ele.quantity + 1;
+                    flag = 1;
+                    // console.log(ele, 'printing inside loop');
+                }
+                
+            })
+            if (flag === 0) {
+                const newProduct = {
+                    pid: pid,
+                    quantity: 1,
+                    title: pdata.header.title,
+                    image: pdata.header.image,
+                    price: pdata.price
+                }
+                currentCart.push(newProduct);
+            }
+            Cart.updateOne({uid : uid},{
+                $set : {
+                    cartItems : currentCart
+                }
+            })
+            .then(resp =>{
+                console.log("Successfully added to cart",resp)
+                res.send(resp);
+            })
+            .catch(err=> {
+                console.log(err);
+                res.send(err);
+            });
+            console.log(currentCart);
+        })
+
+})
+
+app.post("/deleteFromCart", (req, res) => {
+    const pid = req.body.pid;
+    const uid = req.body.uid;
+    let currentCart = [];
+    Cart.findOne({ uid: uid })
+        .then(resp => {
+            currentCart = resp.cartItems;
+        })
+    let flag = 0;
+    currentCart.forEach((ele) => {
+        if (ele.pid === pid) {
+            ele.quantity = ele.quantity - 1;
+            flag = 1;
+        }
+    })
+    if (flag === 0) {
+        const newProduct = {
+            pid: pid,
+            quantity: 1,
+            title: pdata.header.title,
+            image: pdata.header.image,
+            price: pdata.price
+        }
+        currentCart.push(newProduct);
+    }
+    Cart.updateOne({ uid: uid }, {
+        $set: {
+            cartItems: currentCart
+        }
+    })
+        .then(resp => {
+            console.log("Successfully added to cart", resp)
+        })
+        .catch(err => console.log(err));
+})
+
+
+app.post("/users", (req, res) => {
+    let userid = req.body.uid;
+    let email = req.body.email;
+    console.log(userid, email);
+
+    Users.findOne({ uid: userid })
+        .then(resp => {
+            if (resp == undefined) {
+                const User = new Users({
+                    uid: userid,
+                    email: email,
+                    firstName: null,
+                    lastName: null,
+                    mobile: null,
+                    address: null
+                })
+
+                const cart = new Cart({
+                    uid: userid,
+                    cartItems: []
+                })
+                cart.save().then(resp => console.log(resp));
+                User.save().then(doc => res.status(200).json({ message: doc }));
+            }
+            else {
+                res.status(200).json({ message: resp })
+            }
+        })
+        .catch(err => {
+            console.log(err);
+        })
+
+})
+
+
+app.get("/products", (req, res) => {
+    res.status(200).json({ message: products });
+})
+
+
+
+
+app.get("/", (req, res) => {
+
+    res.send("ok");
+
+})
+
+
+app.listen(3000 || process.env.PORT, function () {
+    console.log("Server started on port 3000");
+});
---
+++ products.js
@@ -0,0 +1,66 @@
+const products = [{
+    id: "1",
+    price : 1799,
+    header: {
+        title: "Aaram",
+        description: "Cooling Roll-On",
+        quantity: "10mL | 500 mg cannabinoids",
+        image: ""
+    },
+    about: {
+        title: "Aaram by Gaanj",
+        description: "Aaram means relief in Awadhi. This topical cooling roll-on relieves you of issues like general muscle soreness, spasms, involuntary contractions, migraines, and sinusitis. Packaged in a roll-on bottle, it's easy to carry around and very convenient to use.",
+        image: ""
+    },
+    useCases: {
+        ingredients: "CBD extract; MCT oil; sunflower lecithin, proprietary blend of terpenes, fragrances and essential oils",
+        useCases: "Aaram can be used to manage headaches & migraines, involuntary muscle spasms, post workout soreness and minor sprains & strains.",
+        shipping: "Once you place an order with us, it will typically take 1 business day for our team to process and pack your order. We have a standard fee of INR 150 for all orders. Delivery time for your order will range between 3-7 business days, depending on your location."
+    },
+    testimonials: [{ review: "I get very intense headaches triggered by migraines especially during rainy seasons. I used the cooling roll on to help me with my headaches. I use it before going to work and carry it with me everywhere, it has helped me in a great way. Really amazing product, it’s been very helpful. Completely satisfied with it. Would completely recommend it to anyone and everyone with such conditions.", name: "Madhav Periwal, 26", role: "Business Owner", reason: "Frequent Headaches from Migraines" }, { review: "I used it for chronic headaches that occur due to stress from work & staring at a screen for too long. It provides an immediate relief and a calming sensation when applied on the forehead and temples.", name: "Abhishek, 24", role: "Chef", reason: "Headaches induced from staring at screens" }]
+},
+{
+    id: "2",
+    price : 2799,
+    header: {
+        title: "Vishraam",
+        description: "Heating Roll-on",
+        quantity: "10mL | 1000 mg cannabinoids",
+        image: "",
+    },
+    about: {
+        title: "Vishraam by Gaanj",
+        description: "HVishraam means rest or relaxation in Awadhi. Our transdermal heating roll-on is designed to penetrate through the skin and muscles and reach the bones and nerves to ease the pain. It is easy to carry and use and is highly effective for managing symptoms of arthritis and menstrual cramps.",
+
+        image: "",
+    },
+    useCases: {
+        ingredients: "CBD extract; MCT oil; sunflower lecithin, proprietary blend of terpenes, fragrances and essential oils",
+        useCases: "Vishraam can be used to manage pain & inflammation related to arthritis, fibromyalgia & spondylitis. It's also very helpful with menstrual cramps & severe sprains",
+        shipping: "Once you place an order with us, it will typically take 1 business day for our team to process and pack your order. We have a standard fee of INR 150 for all orders. Delivery time for your order will range between 3-7 business days, depending on your location.",
+    },
+    testimonials: [{
+        review: "I used this to manage knee pain post knee injury. I use it once or twice a week. The heating effect of it replaces the discomfort/pain you feel when it's overextended and provides comfort when I have over worked my knee.",
+        name: "Abheer, 25",
+        role: "Chef",
+        reason: "Post fracture Recovery",
+    }, {
+        review: "I had suffered from an injury last year and because of that I still have recurring back pain. Vishraam really helps in managing that pain, the heating sensation is very soothing but if applied in large quantities it can be uncomfortable so you should use it in lesser quantities.",
+        name: "Angela, 22",
+        role: "Influencer Marketing",
+        reason: "Chronic Back Pain",
+    },
+    {
+        review: "I used it frequently for muscle aches and muscle cramps - almost instant relief in pain but applying too much gives itching/burning sensation on skin, so one should only put one or two lines and then spread & massage it all over the area with pain.",
+        name: "Aastha, 26",
+        role: "Entrepreneur",
+        reason: "Muscle Aches",
+
+    }]
+}]
+
+
+
+
+
+module.exports = {products}
